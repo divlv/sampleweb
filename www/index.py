c57443af60aa6258cfbd5c9b7e5bf5fc6a39711a
dima <dima@zaharov.info>
2022-09-20 19:36:52 +0300

    Using Flask Blueprints to enable Web AND REST in a single application

--- www/index.py
+++ www/index.py
@@ -1,80 +1,80 @@
-from flask import render_template, make_response, abort, Response
-import time
+from flask import Blueprint, render_template
 import datetime
-import re
-import ssl
-from flask import Flask, request
+from flask import Flask
 from flask_cors import CORS
 from flask_restplus import Resource, Api, fields
 
 PATH_TO_TEMPLATES = "./tpl/"
 
-app = Flask(__name__, template_folder=PATH_TO_TEMPLATES)
+# app = Flask(__name__, template_folder=PATH_TO_TEMPLATES)
+app = Flask(__name__)
 
 
-# api = Api(
-#     app,
-#     version="1.0",
-#     title="Sample web REST service",
-#     doc="/api/",
-#     description="Access to the stored data, if any.",
-#     default="Services",
-#     default_label="All available services list:",
-# )
+# Default web page Blueprint START #############################################
+
+default_web = Blueprint(
+    "default_web", __name__, url_prefix="", template_folder=PATH_TO_TEMPLATES
+)
 
 
-# class HealthCheck(Resource):
-#     def get(self):
-#         """
-#         REST Service health check
-#         """
-#         return {
-#             "status": "Sample REST service is up",
-#             "build": "Build number here",
-#             "serverTime": str(datetime.datetime.now()),
-#             "tag": "#GTO",
-#         }
-
-
-# api.add_resource(HealthCheck, "/health")
-
-
-# class HomePage(Resource):
-#     def get(self):
-#         headers = {"Content-Type": "text/html"}
-#         print("home page!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-#         return make_response(
-#             render_template("layout.html", bodyhtml="main"), 200, headers
-#         )
-
-
-# api.add_resource(HomePage, "/")
-
-
-@app.context_processor
+@default_web.context_processor
 def year_for_templates():
     return {"this_year": datetime.datetime.now().year}
 
 
-@app.route("/")
+@default_web.route("/")
 def home_page():
     return render_template("layout.html", bodyhtml="main")
 
 
 # <Error pages>
-@app.errorhandler(404)
+@default_web.app_errorhandler(404)
 def page_not_found(e):
     return render_template("errors/404.html"), 404
 
 
-@app.errorhandler(500)
+@default_web.app_errorhandler(500)
 def internal_server_error(e):
     return render_template("errors/500.html"), 500
 
 
-# </Error pages>
+app.register_blueprint(default_web)
+# Default web page Blueprint FINISH #############################################
+
+
+# REST API Blueprint START ######################################################
+swagger = Blueprint("swagger", __name__, url_prefix="/api")
+
+api = Api(
+    swagger,
+    version="1.0",
+    title="Sample web REST service",
+    description="Access to the stored data, if any.",
+    default="Services",
+    default_label="All available services list:",
+)
+
+
+class HealthCheck(Resource):
+    def get(self):
+        """
+        REST Service health check
+        """
+        return {
+            "status": "Sample REST service is up",
+            "build": "Build number here",
+            "serverTime": str(datetime.datetime.now()),
+            "tag": "#GTO",
+        }
+
+
+api.add_resource(HealthCheck, "/health")
+
+app.register_blueprint(swagger)
+# REST API Blueprint FINISH ######################################################
 
 
 # Uncomment this line, if running in Development environment
 # i.e. not with Gunicorn
+# (For this demo-project - using the Development server)
 app.run(host="0.0.0.0", port="80", threaded=True)
